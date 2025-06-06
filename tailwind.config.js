d58f3103f46da4f740d6dfd7c1b89c2a484cb4b4
Saad Ferozee <161690733+saadferozee@users.noreply.github.com>
2025-06-06 16:03:45 +0600

    4 Section Added

---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+    // module.exports = {
+    //     theme: {
+    //     extend: {
+    //         fontFamily: {
+    //         'Shafkat': ['Shafkat']
+    //         },
+    //     },
+    //     },
+    // }
+
+    // module.exports = {
+    //     theme: {
+    //     extend: {
+    //         fontFamily: {
+    //         'Sora': ['Sora']
+    //         },
+    //     },
+    //     },
+    // }
+
+    // module.exports = {
+    //     theme: {
+    //     extend: {
+    //         fontFamily: {
+    //         'Poppins': ['Poppins']
+    //         },
+    //     },
+    //     },
+    // }
+
+    // module.exports = {
+    //     theme: {
+    //     extend: {
+    //         fontFamily: {
+    //         'PoppinsBold': ['PoppinsBold']
+    //         },
+    //     },
+    //     },
+    // }
